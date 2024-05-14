94c5f913825c1298101ae2e58c65ce5ba8d42e79
Sophia Phu <sophiatiffphu@gmail.com>
2024-05-14 18:28:54 -0500

    upload next project

---
+++ sophia-personal-website/src/app/projects/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Projects() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 lg:p-24 gap-12 bg-[#FBD0DE]">
+      <div>
+        <h1>
+          My Projects :)
+        </h1>
+      </div>
+    </main>
+  );
+}
