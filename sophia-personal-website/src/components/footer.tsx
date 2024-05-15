62318bc4cd434c9c65e813cd3e2adedb337c639e
Sophia Phu <sophiatiffphu@gmail.com>
2024-05-15 18:23:45 -0500

    modified fontsize of hi i'm sophia

---
+++ sophia-personal-website/src/components/footer.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { RxInstagramLogo } from "react-icons/rx";
+import { RxGithubLogo } from "react-icons/rx";
+import { RxLinkedinLogo } from "react-icons/rx";
+import { RxFileText } from "react-icons/rx";
+export default function Footer() {
+  return (
+
+  );
+}
