4a49b8b565e6737798add68b4ef8d748a9affcd1
Sophia Phu <sophiatiffphu@gmail.com>
2024-05-16 03:21:03 -0500

    hopefully completed resume page

--- sophia-personal-website/src/app/resume/page.tsx
+++ sophia-personal-website/src/app/resume/page.tsx
@@ -1,16 +1,25 @@
-import Image from "next/image";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
+import Header from '@/components/header';
+import Footer from '@/components/footer';
 
 export default function Resume() {
+  const pdfUrl = "/sophia_phu_resume.pdf";
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between gap-y-12  px-8 lg:px-24 bg-[#FBD0DE]">
-      <Header/>
-      <div>
-        <h1>
-          Resume
-        </h1>
+    <main className="flex min-h-screen flex-col items-center justify-between gap-y-8 lg:gap-y-12 px-8 lg:px-24 bg-[#FBD0DE]">
+      <Header />
+      <div className="flex flex-col w-full gap-4">
+        <h1 className="font-bold text-3xl lg:text-4xl">Resume</h1>
+        <p>
+          View and download my resume below!
+        </p>
+        <iframe 
+          src={pdfUrl} 
+          className="w-full border-none h-[500px] md:h-[800px] lg:h-[1000px]"
+          title="Resume PDF"
+        />
+        <div ></div>
       </div>
+      
       <Footer />
     </main>
   );
