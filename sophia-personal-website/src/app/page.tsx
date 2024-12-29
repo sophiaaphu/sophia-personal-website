406db83fef2b8a2ac56a35d0460e05eede91bb60
Sophia Phu <sophiatiffphu@gmail.com>
2024-12-29 02:17:51 -0600

    featured should be doneee

--- sophia-personal-website/src/app/page.tsx
+++ sophia-personal-website/src/app/page.tsx
@@ -7,12 +7,13 @@ import {
   RxGithubLogo,
   RxLinkedinLogo,
   RxHeart,
+  RxArrowRight,
 } from "react-icons/rx";
 
 export default function Home() {
   return (
     <main className="flex flex-col py-12 gap-16">
-      <div className="flex flex-col lg:flex-row lg:items-center gap-4 lg:gap-0  ">
+      <div className="flex flex-col md:flex-row lg:items-center gap-4 lg:gap-0  ">
         <div className="flex flex-col gap-6">
           <h1 className=" text-3xl lg:text-4xl font-bold">
             Hi I'm{" "}
@@ -68,8 +69,8 @@ export default function Home() {
       </div>
       <div>
         <p className="text-3xl lg:text-4xl font-bold">Featured</p>
-        <div className="mt-4 flex gap-2">
-          <div className="flex flex-col bg-[#FEEDF5] p-8 rounded-lg w-1/3 justify-between shadow-md">
+        <div className="mt-4 flex flex-col lg:flex-row gap-2">
+          <div className="flex flex-col bg-[#FEEDF5] p-4 md:p-8 rounded-lg lg:w-1/3 justify-between shadow-md">
             <div className=" space-y-4">
               <p className="text-lg font-semibold">AggieSeek</p>
               <p className=" text-sm">
@@ -79,14 +80,23 @@ export default function Home() {
                 1000+ users.
               </p>
             </div>
-            <div>
-              <Button className="bg-[#FEEDF5] text-black border border-[#ABABAB] hover:bg-[#FFDCE8]">
-                Visit AggieSeek
+            <div className=" flex items-center gap-4 mt-8 lg:mt-0">
+              <Button className="bg-[#FEEDF5] text-black border border-[#ABABAB] hover:bg-[#FFDCE8] space-x-2 hover:space-x-6 ">
+                <p>Visit AggieSeek</p>
+                <RxArrowRight />
               </Button>
+              <a
+                href="https://github.com/sophiaaphu"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:underline"
+              >
+                <RxGithubLogo className="w-5 h-5" />
+              </a>
             </div>
           </div>
-          <div className="flex gap-8 bg-[#DAB6BC] px-8 rounded-lg w-2/3 shadow-md pt-8">
-            <div className="flex items-end">
+          <div className="flex gap-4 md:gap-8 bg-[#DAB6BC] sm:px-4 md:px-8 rounded-lg lg:w-2/3 shadow-md pt-4 md:pt-8 group overflow-hidden">
+            <div className="flex -ml-4 sm:-ml-0 items-end transform transition-transform duration-300 group-hover:scale-105">
               <Image
                 src="/aggie-seek-new-1.png"
                 alt="aggie seek 1"
@@ -95,7 +105,7 @@ export default function Home() {
                 className=" rounded-t-lg"
               />
             </div>
-            <div className="flex items-end">
+            <div className="flex -mr-4 sm:-mr-0 items-end transform transition-transform duration-300 group-hover:scale-105">
               <Image
                 src="/aggie-seek-new-2.png"
                 alt="aggie seek 2"
@@ -106,9 +116,9 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="mt-2 flex gap-2">
-          <div className="flex gap-8 bg-[#DAB6BC] px-8 rounded-lg w-2/3 shadow-md pt-8">
-            <div className="flex items-end">
+        <div className="mt-2 flex flex-col-reverse lg:flex-row gap-2">
+          <div className="flex gap-4 md:gap-8 bg-[#DAB6BC] sm:px-4 md:px-8 rounded-lg lg:w-2/3 shadow-md pt-4 md:pt-8 group overflow-hidden">
+            <div className="flex -ml-4 sm:-ml-0 items-end transform transition-transform duration-300 group-hover:scale-105">
               <Image
                 src="/tao-website-1.png"
                 alt="tao website 1"
@@ -117,7 +127,7 @@ export default function Home() {
                 className=" rounded-t-lg"
               />
             </div>
-            <div className="flex items-end">
+            <div className="flex -mr-4 sm:-mr-0 items-end transform transition-transform duration-300 group-hover:scale-105">
               <Image
                 src="/tao-website-2.png"
                 alt="tao website 2"
@@ -127,17 +137,29 @@ export default function Home() {
               />
             </div>
           </div>
-          <div className="flex flex-col bg-[#FEEDF5] p-8 rounded-lg w-1/3 justify-between shadow-md">
-            <div className=" space-y-4">
+          <div className="flex flex-col bg-[#FEEDF5] p-4 md:p-8 rounded-lg lg:w-1/3 justify-between shadow-md">
+            <div className="space-y-4">
               <p className="text-lg font-semibold">TAO Website</p>
-              <p className=" text-sm">
-                The TAO website displays essential resources for 4000+ freshman engineers going through intro engineering coursework. It displays TAO events and announcements, TAO social media links, and review materials. 
+              <p className=" text-sm ">
+                The TAO website displays essential resources for 4000+ freshman
+                engineers going through intro engineering coursework. It
+                displays TAO events and announcements, TAO social media links,
+                and review materials.
               </p>
             </div>
-            <div>
-              <Button className="bg-[#FEEDF5] text-black border border-[#ABABAB] hover:bg-[#FFDCE8]">
-                Visit the TAO Website
+            <div className=" flex items-center gap-4 mt-8 lg:mt-0">
+              <Button className="bg-[#FEEDF5] text-black border border-[#ABABAB] hover:bg-[#FFDCE8] space-x-2 hover:space-x-6">
+                <p>Visit TAO Website</p>
+                <RxArrowRight />
               </Button>
+              <a
+                href="https://github.com/sophiaaphu"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:underline"
+              >
+                <RxGithubLogo className="w-5 h-5" />
+              </a>
             </div>
           </div>
         </div>
