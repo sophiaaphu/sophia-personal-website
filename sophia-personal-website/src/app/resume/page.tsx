356ae183ef51687e842f6b6b762d02889f608a13
Sophia Phu <sophiatiffphu@gmail.com>
2024-12-29 19:09:44 -0600

    made resume tab redirect to google drive link

--- sophia-personal-website/src/app/resume/page.tsx
+++ sophia-personal-website/src/app/resume/page.tsx
@@ -1,26 +1,19 @@
-import Header from '@/components/header';
-import Footer from '@/components/footer';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Resume() {
-  const pdfUrl = "https://drive.google.com/file/d/1l_ZYUjVsBTHMnOFywGmIeic6DmtWhvG_/preview";
+  const router = useRouter();
+
+  useEffect(() => {
+    // Open the resume link in a new tab
+    window.open('https://drive.google.com/file/d/1l_ZYUjVsBTHMnOFywGmIeic6DmtWhvG_/preview', '_blank', 'noopener,noreferrer');
+    // Redirect to the home page after opening the new tab
+    router.replace('/');
+  }, [router]);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between gap-y-8 lg:gap-y-12 px-8 lg:px-24 bg-[#FBD0DE]">
-      <Header />
-      <div className="flex flex-col w-full gap-4">
-        <h1 className="font-bold text-3xl lg:text-4xl">Resume</h1>
-        <p>
-          View and download my resume below!
-        </p>
-        <iframe 
-          src={pdfUrl} 
-          className="w-full border-none h-[500px] md:h-[800px] lg:h-[1000px] rounded-xl"
-          title="Resume PDF"
-        />
-        <div ></div>
-      </div>
-      
-      <Footer />
-    </main>
+    <div>
+      <p>Redirecting to resume...</p>
+    </div>
   );
 }
